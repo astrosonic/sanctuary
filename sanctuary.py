173c82a9b69f983d5ae44f504c185a1482b7dd80
xcodz-dot <xcodz-dot@users.noreply.github.com>
2020-12-27 12:33:51 +0530

    Added the Ip address assignment support

--- sanctuary.py
+++ sanctuary.py
@@ -2,6 +2,7 @@ from multiprocessing import Process
 
 import click
 import colorama
+import socket
 
 colorama.init()  # Initialize the console colors on windows
 
@@ -40,16 +41,27 @@ import servchat
     help="Start the server on an IPv4 address",
     required=True,
 )
+@click.option(
+    "-i",
+    "--ip",
+    "ip_address",
+    required=False,
+    help="Change the Server IP, use value 'auto' for ipv4 to automatically assign a address",
+    default=None
+)
 @click.version_option(
     version="22072020", prog_name="Sanctuary by AstroSonic"
 )
-def main(chatport, netprotc, servport):
+def main(chatport, netprotc, servport, ip_address):
+    if ip_address == "auto" and netprotc == "ipprotv4":
+        ip_address = socket.gethostbyname(socket.gethostname())
+
     # Additional Thread
-    server_thread = Process(target=chatroom.mainfunc, args=(chatport, netprotc))
+    server_thread = Process(target=chatroom.mainfunc, args=(chatport, netprotc, ip_address))
     server_thread.start()
 
     # Main Thread
-    servchat.mainfunc(servport, chatport, netprotc)
+    servchat.mainfunc(servport, chatport, netprotc, ip_address)
 
     server_thread.terminate()
 
